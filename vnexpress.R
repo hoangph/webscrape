6fabc4cf5bf5a663b48ff76a83477ff64b8c8147
Hoangph <hoangkidieu@gmail.com>
2016-09-24 12:15:44 +0700

    vnexpress final ver 1

--- vnexpress.R
+++ vnexpress.R
@@ -7,8 +7,10 @@ library(readr)
 library(tidyverse)
 library(Rfacebook)
 library(rvest)
-
-
+library(lubridate)
+library(stringr)
+dir = "C:/Users/Windows/Dropbox/DEPOCEN/Scrape/webscrape"
+setwd(dir)
 ##########################
 #####  Vnexpress    ######
 ##########################
@@ -33,34 +35,79 @@ clean_date = function (date) {
   return(date_d)
 }
 
-
-read_page = function(start, end, link, selector, date_selector) {
+# Read article's content
+read_page = function(start_date, end_date, link, content_selector, date_selector) {
   result = c()
-  error = 0
+  stop = 0
   for (i in c(1:length(link))) {
-    url = link[i]
+    url = as.character(link[i])
     html = read_html(url)
-    date = html %>% html_nodes(date_selector) %>% html_text() %>% clean_date()
-    if (date >= start & date <= end) {
-      paragraph = html %>% html_nodes(selector) %>% html_text() %>% paste(collapse = " ")
-      result = c(result, paste(date, paragraph, sep = "$:"))
+    ar_date = html %>% html_nodes(date_selector) %>% html_text()%>%
+            paste(collapse = "") %>% clean_date()
+    message("Scraping date: ", ar_date, ", link: ", url)
+    if (ar_date >= start_date & ar_date <= end_date) { #get articles in the time range
+      paragraph = html %>% html_nodes(content_selector) %>% 
+        html_text() %>% paste(collapse = " ")
     } else { 
-      error = 1
+      stop = 1 #signal to stop outside loop
       break
     }
+    result = rbind(result,data.frame(ar_date,paragraph))
   }
-  return(list(result, error))
+  return(list(result,stop))
 }
 
-scrape_news = function (source, start, end, selector, date_selector, link_selector) {
-  stop = 0
+# Loop for scraping execution
+scrape_news = function (source, source_suffix,
+                        start_date, end_date, content_selector, 
+                        date_selector, link_selector, save_dir) {
   i = 1
-  content = c()
+  output = c()
   while (1) {
-    link = source %>% paste(i,".html", sep = "") %>% get_link(link_selector)
-    output = read_page(start, end, link, selector, date_selector)
-    content = c(content, output[[1]])
-    if (output[2] == 1) {break} else {i=i+1}
+    message("Scraping page ", i)
+    link_table = source %>% paste(i,source_suffix, sep = "") %>% 
+      get_article(article_selector)
+    link = link_table$article_link
+    content = read_page(start_date, end_date, link, content_selector, date_selector)
+    sum_table = cbind(link_table[1:nrow(content[[1]]),],content[[1]])
+    output = rbind(output, sum_table)
+    if (content[[2]] == 1) {break} else {i=i+1}
   }
-  return(content)
+  #Save
+  save(output, save_dir)
+  return(output)
 }
+
+save = function(file, save_dir) {
+  setwd(save_dir)
+  write_excel_csv(as.data.frame(file), 
+                  paste(as.character(today()),
+                        as.character(start_date),
+                        as.character(end_date),".csv",sep = "_"))
+}
+
+##############################
+####### __Execution ##########
+##############################
+
+# ___Scrape chuyen muc Phap luat####
+# Parameter
+source = "http://vnexpress.net/tin-tuc/phap-luat/page/"
+source_suffix = ".html"
+start_date = today()
+end_date = today()
+content_selector = ".short_intro , .Normal"
+date_selector = ".block_timer"
+article_selector = "#news_home .txt_link"
+# Save directory
+save_dir = paste(dir,"/vnexpress",sep="")
+
+# Call function
+# Parameters required: 
+#   source, source_suffix, start_date, end_date, content_selector, 
+#   date_selector, link_selector
+timestart = now()
+final = scrape_news(source, source_suffix, start_date, end_date, content_selector, 
+            date_selector, link_selector, save_dir)
+print(now()-timestart)
+
